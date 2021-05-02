e59ccb5b361e00dec6e5217891dd856da740bad7
Gagan Malvi <malvi@aospa.co>
2021-05-02 11:39:38 +0200

    mtk-drivers: video: mt6765: nuke debugging
    Signed-off-by: Gagan Malvi <malvi@aospa.co>
    Signed-off-by: Azuki <id_azuki@protonmail.com>

--- drivers/misc/mediatek/video/mt6765/dispsys/ddp_log.h
+++ drivers/misc/mediatek/video/mt6765/dispsys/ddp_log.h
@@ -56,7 +56,7 @@
 #define DISP_LOG_E(fmt, args...)					\
 	do {								\
 		dprec_logger_pr(DPREC_LOGGER_ERROR, fmt, ##args);	\
-		pr_info("[DDP/"LOG_TAG"]error:"fmt, ##args);		\
+		pr_debug("[DDP/"LOG_TAG"]error:"fmt, ##args);		\
 	} while (0)
 
 #define DDPIRQ(fmt, args...)						\
@@ -83,7 +83,7 @@
 		} else {						\
 			dprec_logger_pr(DPREC_LOGGER_DUMP, \
 				fmt, ##__VA_ARGS__);	\
-			pr_info("[DDP/"LOG_TAG"]"fmt, \
+			pr_debug("[DDP/"LOG_TAG"]"fmt, \
 				##__VA_ARGS__);	\
 		}					\
 	} while (0)
@@ -93,7 +93,7 @@
 	do {						\
 		if (expr)				\
 			break;				\
-		pr_info("DDP ASSERT FAILED %s, %d\n", __FILE__, __LINE__); \
+		pr_debug("DDP ASSERT FAILED %s, %d\n", __FILE__, __LINE__); \
 		WARN_ON(1);\
 	} while (0)
 #endif
@@ -106,14 +106,14 @@
 		aee_kernel_warning_api(__FILE__, __LINE__,		\
 			DB_OPT_DEFAULT | DB_OPT_MMPROFILE_BUFFER, str, \
 			string, ##args);	\
-		pr_info("[DDP Error]"string, ##args);			\
+		pr_debug("[DDP Error]"string, ##args);			\
 	} while (0)
 #else
 #define DDPAEE(string, args...)						\
 	do {								\
 		char str[200];						\
 		snprintf(str, 199, "DDP:"string, ##args);		\
-		pr_info("[DDP Error]"string, ##args);			\
+		pr_debug("[DDP Error]"string, ##args);			\
 	} while (0)
 #endif
 
