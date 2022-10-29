837d2940957478a9292fd3cf0b863c686d5484ab
wulan17 <wulan17@nusantararom.org>
2022-10-29 11:18:41 +0700

    tools: dct: Use python3
    Python2 already EOL on Jan 2020 and some linux distribution like archlinux already remove python2 from their repository
    
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- tools/dct/utility/util.py
+++ tools/dct/utility/util.py
@@ -28,21 +28,17 @@ class LogLevel:
 CURRENT_LEVEL = LogLevel.error
 
 def log(level, msg):
-    if level >= CURRENT_LEVEL:
-        if level == LogLevel.info:
-            print LEVEL_INFO + msg
-        elif level == LogLevel.warn:
-            print LEVEL_WARN + msg
-        elif level == LogLevel.error:
-            print LEVEL_ERROR + msg
-
-def set_level(value):
-    CURRENT_LEVEL = value
+    if level == LogLevel.info:
+        print(LEVEL_INFO + msg)
+    elif level == LogLevel.warn:
+        print(LEVEL_WARN + msg)
+    elif level == LogLevel.error:
+        print(LEVEL_ERROR + msg)
 
 def compare(value):
     lst = re.findall(r'\d+', value)
     if len(lst) != 0:
-        return string.atoi(lst[0])
+        return int(lst[0])
 
     # if can not find numbers
     return value
