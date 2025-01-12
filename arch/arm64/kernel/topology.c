4372ccb946cfd040e5d484ed8c67854408c0ba71
Hadenix <vsegda_online_007@bk.ru>
2025-01-12 07:19:36 +0300

    include: fix warning: 'const' type qualifier on return type has no effect
    ../include/linux/sched/sched.h:107:35: warning: 'const' type qualifier on return type has no effect [-Wignored-qualifiers]
    const struct sched_group_energy * const cpu_core_energy(int cpu);
                                      ^~~~~~
    ../include/linux/sched/sched.h:110:35: warning: 'const' type qualifier on return type has no effect [-Wignored-qualifiers]
    const struct sched_group_energy * const cpu_cluster_energy(int cpu);
                                      ^~~~~~
    ../include/linux/sched/sched.h:113:42: warning: 'const' type qualifier on return type has no effect [-Wignored-qualifiers]
    extern const struct sched_group_energy * const cci_energy(void);
                                             ^~~~~~
    ../include/linux/sched.h:1337:35: warning: 'const' type qualifier on return type has no effect [-Wignored-qualifiers]
    const struct sched_group_energy * const(*sched_domain_energy_f)(int cpu);
                                      ^~~~~
    
    ../arch/arm64/kernel/topology.c:241:41: error: conflicting types for 'cpu_cluster_energy'
    const struct sched_group_energy * const cpu_cluster_energy(int cpu)
                                            ^
    ../arch/arm64/kernel/topology.c:271:41: error: conflicting types for 'cpu_core_energy'
    const struct sched_group_energy * const cpu_core_energy(int cpu)
                                            ^

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -238,7 +238,7 @@ EXPORT_SYMBOL_GPL(cpu_topology);
 
 /* sd energy functions */
 inline
-const struct sched_group_energy * const cpu_cluster_energy(int cpu)
+const struct sched_group_energy * cpu_cluster_energy(int cpu)
 {
 	struct sched_group_energy *sge = sge_array[cpu][SD_LEVEL1];
 #ifdef CONFIG_MTK_UNIFY_POWER
@@ -268,7 +268,7 @@ const struct sched_group_energy * const cpu_cluster_energy(int cpu)
 }
 
 inline
-const struct sched_group_energy * const cpu_core_energy(int cpu)
+const struct sched_group_energy * cpu_core_energy(int cpu)
 {
 	struct sched_group_energy *sge = sge_array[cpu][SD_LEVEL0];
 #ifdef CONFIG_MTK_UNIFY_POWER
